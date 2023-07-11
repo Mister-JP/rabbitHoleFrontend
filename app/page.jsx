4065b8ce68ec23144081bfecd2d0f63f9f537b5e
Jignasu Pathak <jignasupathak@Jignasus-MacBook-Pro.local>
2023-07-11 18:28:44 -0400

    Accidental artist was awake

--- app/page.jsx
+++ app/page.jsx
@@ -227,8 +227,9 @@ export default function Home() {
               <h1 style={{textShadow: "2px 2px #000"}} className="font-plusJakarta text-white text-2xl md:text-6xl font-semibold">Explore, Engage, Enjoy: </h1>
               <h1 style={{textShadow: "2px 2px #000"}} className="font-plusJakarta text-white text-2xl md:text-6xl font-semibold">Welcome to Your Movie Community</h1>
               <p style={{textShadow: "2px 2px #000"}} className="mt-5 font-plusJakarta text-white text-l md:text-xl">Recommendations are made by movie lovers like you. Engage in threaded discussions, connect over suggestions, and broaden your cinematic horizons.</p>
-              <button onClick={() => handleButtonClick("register")} className="mt-5 py-2 px-4 rounded border border-gray rounded-full font-bold hover:bg-white hover:text-black text-white transition duration-200 w-full md:w-[200px]">Sign up now!</button>
+              
           </div>
+          <button onClick={() => handleButtonClick("register")} className="mt-5 absolute -right-40 py-2 px-4 text-6xl rounded border border-gray rounded-full font-bold hover:bg-white hover:text-black text-gray-100 drop-shadow-xl transition duration-200 h-full w-full md:w-[200px]">Sign up now!</button>
             </div>
         </div>
         <img src="/imgs/community.png" className="object-cover h-full w-2/5"/>
